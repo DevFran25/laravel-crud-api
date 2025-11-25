21d21f22c7a78d5ade24d2db8326e787aaf611ce
Francisco Collantes <developerfran25@gmail.com>
2025-11-24 23:16:59 -0500

    Implementar PlayerController con endpoints para listar y crear jugadores

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Http\Controllers\Api\playerController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
