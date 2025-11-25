faefdbfd6bd6ad9630388264ff817726196100c6
Francisco Collantes <developerfran25@gmail.com>
2025-11-24 23:22:38 -0500

    Agregar rutas API base para Player

--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,21 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use Illuminate\Http\Controllers\Api\playerController;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+Route::get('/players', [playerController::class, 'index']);
+
+Route::get('/players/{id}', function (){
+    return 'Obteniendo jugador por ID';
+});
+
+Route::post('/players', function (){
+    return 'Creando un nuevo jugador';
+});
+
+Route::put('/players/{id}', function (){
+    return 'Actualizando un jugador por ID';
+});
+
+Route::delete('/players/{id}', function (){
+    return 'Eliminando un jugador por ID';
+});
+
